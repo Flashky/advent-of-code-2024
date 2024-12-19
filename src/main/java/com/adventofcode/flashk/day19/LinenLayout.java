6a0a3b4a310933c41567d62a112c94d150034370
Flashky <mevengar@gmail.com>
2024-12-19 11:30:49 +0100

    feat: restore day 19 part 1

--- src/main/java/com/adventofcode/flashk/day19/LinenLayout.java
+++ src/main/java/com/adventofcode/flashk/day19/LinenLayout.java
@@ -33,35 +33,7 @@ public class LinenLayout {
         }
         return result;
     }
-
-    public int solveB() {
-        int result = 0;
-        for(String design : designs) {
-            isPossible2(design, design);
-        }
-        return 0;
-    }
-
-    /*
-    private int countPossible(String design) {
-        if(memo2.containsKey(design)) {
-            memo2.put(design, memo2.get(design)+1);
-        }
-
-        if(StringUtils.EMPTY.equals(design)) {
-            return 1;
-        }
-
-        for(String pattern : patterns) {
-            int index = design.indexOf(pattern);
-
-            String leftSide = design.substring(0, index);
-            String rightSide = design.substring(index+pattern.length());
-            memo.put(leftSide, isPossible(leftSide));
-            memo.put(rightSide, isPossible(rightSide));
-        }
-    }*/
-
+    
     private boolean isPossible(String design){
 
         if(memo.containsKey(design)) {
@@ -83,35 +55,6 @@ public class LinenLayout {
 
                 if(memo.get(leftSide) && memo.get(rightSide)) {
                     return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    private boolean isPossible2(String targetDesign, String design){
-
-        if(memo.containsKey(design)) {
-            return memo.get(design);
-        }
-
-        if(StringUtils.EMPTY.equals(design)) {
-            return true;
-        }
-
-        for(String pattern : patterns) {
-            int index = design.indexOf(pattern);
-            if(index != - 1) {
-                String leftSide = design.substring(0, index);
-                String rightSide = design.substring(index+pattern.length());
-
-                memo.put(leftSide, isPossible2(targetDesign, leftSide));
-                memo.put(rightSide, isPossible2(targetDesign, rightSide));
-
-                if(memo.get(leftSide) && memo.get(rightSide)) {
-                    int count = memo2.getOrDefault(targetDesign, 0);
-                    memo2.put(targetDesign, count+1);
                 }
             }
         }
